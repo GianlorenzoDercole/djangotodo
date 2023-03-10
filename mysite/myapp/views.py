2b30a204851a65e3c4c214570634f72cd354ed3a
GianlorenzoDercole <johndercole01@gmail.com>
2023-03-10 15:50:32 -0800

    assignment form is functional see views.py and index.html

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 from .forms import AssignmentForm
+from .models import Assignment
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        assignment = AssignmentForm(request.POST)
+        if assignment.is_valid():
+            assignment.save()
+
+    assignments = Assignment.objects.all()
     assignment_form = AssignmentForm()
-    return render(request,'myapp/index.html',{'assignment_form':assignment_form})
+    return render(request,'myapp/index.html',{'assignment_form':assignment_form,'assignments':assignments})
