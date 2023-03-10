64d97afa810cabcd15522c18074236f560122d9f
GianlorenzoDercole <johndercole01@gmail.com>
2023-03-10 15:30:37 -0800

    added assignment form see views.py and index.html

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+from .forms import AssignmentForm
 # Create your views here.
 
 def index(request):
-    return render(request,'myapp/index.html')
+    assignment_form = AssignmentForm()
+    return render(request,'myapp/index.html',{'assignment_form':assignment_form})
