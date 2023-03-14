11701aea2933d28597fc2900b1f36e87ae60ddb1
GianlorenzoDercole <johndercole01@gmail.com>
2023-03-14 11:54:56 -0700

    added delete functionality see index.html line 12 and views.py line 22

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
 from .forms import AssignmentForm
 from .models import Assignment
 # Create your views here.
-
 def index(request):
     if request.method == "POST":
         assignment = AssignmentForm(request.POST)
         if assignment.is_valid():
             assignment.save()
+            return redirect('index')
 
     assignments = Assignment.objects.all()
     assignment_form = AssignmentForm()
     return render(request,'myapp/index.html',{'assignment_form':assignment_form,'assignments':assignments})
+
+
+
+
+
+
+def delete(request,id):
+    if request.method =='POST' and 'delete' in request.POST:
+        assignment = Assignment.objects.get(id=id)
+        assignment.delete()
+
+    return redirect('index')
